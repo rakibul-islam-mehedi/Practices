216674522d7b0c880e41520745da42f82fecee8e
lamusoft-technology <rakibulislammehedi4@gmail.com>
2024-03-15 16:34:23 +0600

    s
    Signed-off-by: lamusoft-technology <rakibulislammehedi4@gmail.com>

--- app/src/main/java/com/lamusoft/season2practice/MainActivity.java
+++ app/src/main/java/com/lamusoft/season2practice/MainActivity.java
@@ -1,12 +1,12 @@
 package com.lamusoft.season2practice;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         initialization();
 
